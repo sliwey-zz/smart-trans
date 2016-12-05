2b858e1089b0e17029ddc40f3bbd0aa9520cc6d3
sliwey <qlw1009@gmail.com>
2016-12-05 17:24:56 +0800

    commit

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import Login from './components/login';
 import Home from './components/home';
 
 import { Provider } from 'react-redux'
@@ -14,7 +13,6 @@ ReactDOM.render((
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={Home} />
-      <Route path="/login" component={Login} />
     </Router>
   </Provider>
 ), document.getElementById('app'));
--- app/components/home/index.jsx
+++ app/components/home/index.jsx
@@ -1,12 +1,8 @@
 import Rx from 'rxjs/Rx';
-// import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 // import ReactDOM from 'react-dom';
-import Header from '../header';
-import Asider from '../asider';
 import './home.scss';
-
-import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
 
 import { addTodo, completeTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from '../../actions';
 import AddTodo from '../todo/AddTodo'
@@ -16,8 +12,8 @@ import Footer from '../todo/Footer'
 class App extends Component {
 
   componentDidMount() {
-    const searchKey = document.getElementById('searchKey');
-    const searchBtn = document.getElementById('searchBtn');
+    // const searchKey = document.getElementById('searchKey');
+    // const searchBtn = document.getElementById('searchBtn');
     let map = new AMap.Map('map',{
       zoom: 10
     });
@@ -28,27 +24,27 @@ class App extends Component {
 
     // let searchBtn$ = Rx.Observable.fromEvent(searchBtn, 'click');
 
-    Rx.Observable
-      .fromEvent(searchKey, 'keyup')
-      .debounceTime(250)
-      .pluck('target', 'value')
-      .switchMap(value => fetch(`http://restapi.amap.com/v3/place/text?key=539e96e62a32d7818e821d724052cd81&city=宁波&offset=5&s=rsv3&keywords=${value}`))
-      .subscribe(res => {
-        if (res.ok) {
-          res.json().then(data => {
-            console.log(data)
-          })
-        }
-      });
+    // Rx.Observable
+    //   .fromEvent(searchKey, 'keyup')
+    //   .debounceTime(250)
+    //   .pluck('target', 'value')
+    //   .switchMap(value => fetch(`http://restapi.amap.com/v3/place/text?key=539e96e62a32d7818e821d724052cd81&city=宁波&offset=5&s=rsv3&keywords=${value}`))
+    //   .subscribe(res => {
+    //     if (res.ok) {
+    //       res.json().then(data => {
+    //         console.log(data)
+    //       })
+    //     }
+    //   });
 
-    Rx.Observable
-      .fromEvent(searchBtn, 'click')
-      .subscribe(value => {
-        let count = this.state.count;
+    // Rx.Observable
+    //   .fromEvent(searchBtn, 'click')
+    //   .subscribe(value => {
+    //     let count = this.state.count;
 
-        count++;
-        this.setState({count: count});
-      });
+    //     count++;
+    //     this.setState({count: count});
+    //   });
 
     // Rx.Observable.merge(searchKey$, searchBtn$).subscribe(x => console.log(x));
 
@@ -59,34 +55,7 @@ class App extends Component {
     const { dispatch, visibleTodos, visibilityFilter } = this.props
     return (
       <div className="wrap">
-        <Header />
-        <Asider />
-        <div className="container">
-          <div className="search-wrap">
-            <input id="searchKey" type="text" className="search-input"/>
-            <button id="searchBtn" className="search-btn">search</button>
-          </div>
-          <ul>
-            <li></li>
-          </ul>
-          <div>
-            <AddTodo
-              onAddClick={text =>
-                dispatch(addTodo(text))
-              } />
-            <TodoList
-              todos={visibleTodos}
-              onTodoClick={index =>
-                dispatch(completeTodo(index))
-              } />
-            <Footer
-              filter={visibilityFilter}
-              onFilterChange={nextFilter =>
-                dispatch(setVisibilityFilter(nextFilter))
-              } />
-          </div>
-          <div id="map" style={{width: '300px', height: '150px'}}></div>
-        </div>
+        <div id="map" className="map"></div>
       </div>
     )
   }
